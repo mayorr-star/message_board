a8dc0ad94e7e1df237cfe27ad1fc4129b1d320c5
mayorr-star <immaboat11@gmail.com>
2024-09-03 21:00:17 +0000

    Initialize app as the express server and ejs as template engine

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const indexRouter = require('./routes/index');
+
+app.use('/', indexRouter);
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
