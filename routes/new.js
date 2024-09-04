2aecb15310da68e837056237fcc6791341781c90
mayorr-star <immaboat11@gmail.com>
2024-09-04 21:05:57 +0000

    Add a new route to get individual message

--- routes/new.js
+++ routes/new.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
 const router = Router();
 const getForm = require('../controllers/formController');
+const messageController = require('../controllers/messageController');
 
 router.get('/', getForm);
+router.get('/:messageId', messageController.getMessageById);
 
 module.exports = router;
