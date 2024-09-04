6897baf623091ffab818e68179d1561e3a1c376a
mayorr-star <immaboat11@gmail.com>
2024-09-04 16:33:05 +0000

    Use express urlencoded middleware
    Pass form data into request body

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
 
 const indexRouter = require('./routes/index');
+const newMessageRouter = require('./routes/new');
 
 app.use('/', indexRouter);
+app.use('/new', newMessageRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
