8c774a5edfbdc84dddf3c61a95668dad4fcda4ad
mayorr-star <immaboat11@gmail.com>
2024-09-05 21:06:44 +0000

    Add a required attribute to input and textarea
    Prevent user from submitting form without the necessary information

--- utilis/error_handling/middlewares/notFoundError.js
+++ utilis/error_handling/middlewares/notFoundError.js
@@ -1,4 +1,4 @@
-const notFoundError = (err, req, res, next) => {
+const handleNotFoundError = (err, req, res, next) => {
   if (err.name === "NotFoundError") {
     console.log(err);
     res.status(404).send("Messages not found");
@@ -7,9 +7,4 @@ const notFoundError = (err, req, res, next) => {
   }
 };
 
-const globalErrorHandler = (err, req, res, next) => {
-    console.log(err);
-    res.status(500).send('Something went wrong!');
-};
-
-module.exports = { notFoundError, globalErrorHandler };
+module.exports = handleNotFoundError;
