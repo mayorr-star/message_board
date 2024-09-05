ebba9729b2bbe907bb6d6fd1f2b864757abcbbed
mayorr-star <immaboat11@gmail.com>
2024-09-05 21:14:35 +0000

    Require handleNotFoundError and handleGlobalError error middlewares

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const {
-  notFoundError,
-  globalErrorHandler,
-} = require("./utilis/error_handling/errors/middlewares/notFoundError");
+const handleNotFoundError = require("./utilis/error_handling/middlewares/notFoundError");
+const handleGlobalError = require("./utilis/error_handling/middlewares/globalErrorHandler");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -16,8 +14,8 @@ const newMessageRouter = require("./routes/new");
 app.use("/", indexRouter);
 app.use("/new", newMessageRouter);
 
-app.use(notFoundError);
-app.use(globalErrorHandler);
+app.use(handleNotFoundError);
+app.use(handleGlobalError);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
