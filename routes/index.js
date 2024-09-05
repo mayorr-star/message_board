3781b554aa9f8f86e5c77fc6a26f4ec5fc8a408c
mayorr-star <immaboat11@gmail.com>
2024-09-05 21:09:20 +0000

    Add new route to get a specific message

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const messageController = require('../controllers/messageController');
 const router = Router();
 
 router.get('/', messageController.getMessages);
+router.get('/message/:messageId', messageController.getMessageById);
+
 router.post('/new', messageController.postMessage);
 
 module.exports = router;
